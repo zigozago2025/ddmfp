4484776ee095005abfbaa528960706aba0921c09
zigozago2025 <zigozago_2025@libero.it>
2025-05-22 11:48:27 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 import time
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://cucas2025-sancho.hf.space"     # non mettere lo / finale al link
+MFPPSW = "Cuca1989!"
 
 # Constants
 REFERER = "forcedtoplay.xyz"
