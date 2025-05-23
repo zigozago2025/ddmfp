2ce9288b8d8a4622c85b9a7e67b47741d84ce7cb
zigozago2025 <zigozago_2025@libero.it>
2025-05-23 14:09:39 +0200

    Update hat.py

--- hat.py
+++ hat.py
@@ -10,8 +10,8 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "LINKMFP"
-MFPPSW = "MFPPSW"
+MFPLINK = "https://cucas2025-zigozago.hf.space"
+MFPPSW = "Cuca1989!"
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
     """Estrae il link MPD da una pagina HTML che contiene un iframe con player.html#"""
