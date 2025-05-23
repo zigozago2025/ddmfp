87d3baa9dabba5dc5ba1e8b3164683c8a5be37c3
zigozago2025 <zigozago_2025@libero.it>
2025-05-23 14:05:50 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://cucas2025-zigozago.hf.space"     # non mettere lo / finale al link
+MFPPSW = "Cuca1989!"
 
 # Costanti
 NUM_CHANNELS = 10000
